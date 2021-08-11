2456393aca313ad2efe371c802ea1429d842fd57
lucastu <86346920+lucastu@users.noreply.github.com>
2021-08-11 09:28:38 +0200

    Update canmonitor_edit.py

--- canmonitor/canmonitor_edit.py
+++ canmonitor/canmonitor_edit.py
@@ -17,6 +17,27 @@ can_messages_lock = threading.Lock()
 
 thread_exception = None
 
+#Variables de l'ID des types de frames
+INIT_STATUS_FRAME    = 0x00
+VOLUME_FRAME         = 0x01
+TEMPERATURE_FRAME    = 0x02
+RADIO_SOURCE_FRAME   = 0x03
+RADIO_NAME_FRAME     = 0x04
+RADIO_FREQ_FRAME     = 0x05
+RADIO_FMTYPE_FRAME   = 0x06
+RADIO_DESC_FRAME     = 0x07
+INFO_MSG_FRAME       = 0x08
+RADIO_STATIONS_FRAME = 0x09
+SEATBELTS_FRAME      = 0x0A
+AIRBAG_STATUS_FRAME  = 0x0B
+INFO_TRIP1_FRAME     = 0x0C
+INFO_TRIP2_FRAME     = 0x0D
+INFO_INSTANT_FRAME   = 0x0E
+TRIP_MODE_FRAME      = 0x0F
+AUDIO_SETTINGS_FRAME = 0x10
+SECRET_FRAME         = 0x42 
+
+#Paramètres de la lecture USB
 baudrate = 115200
 serial_device = "/dev/ttyUSB0"
 
@@ -33,19 +54,42 @@ def reading_loop(source_handler, root):
 
             # Add the frame to the can_messages dict and tell the main thread to refresh its content
             with can_messages_lock:
-                try:
-                    root.RadioName.setText("placeholder")
-                except:
-                    print ("pété")
-
+                #root.RadioName.setText("placeholder")
                 can_messages[frame_id] = data
-                print ("FRAME ID %s  :  %s  %s"   % (frame_id, format_data_hex(data),format_data_ascii(data)))
-                if frame_id == 0x02 :
-                    root.Temperature.setText(format_data_hex(data))
 
-                if frame_id == 0x04 :
+                if frame_id == VOLUME_FRAME :
+                    True
+                    #VOLUME = int(format_data_hex(data),16)
+                    #ICI DECLENCHER LE CHANGEMENT DE VOLUME
+                    
+                if frame_id == TEMPERATURE_FRAME :
+                   # root.Temperature.setText(format_data_hex(data))
+                    root.Temperature.setText(int(format_data_hex(data),16))
+                    
+                if frame_id == RADIO_NAME_FRAME :
                     root.RadioName.setText(format_data_ascii(data))
-
+                    
+                if frame_id == RADIO_FREQ_FRAME :
+                    root.RadioFreq.setText(format_data_ascii(data))  
+                    
+                if frame_id == RADIO_FMTYPE_FRAME :
+                    temp = (format_data_hex(data))  
+                    #if temp ==1
+                    #        0 - ---
+                    #        1 - FM1
+                    #        2 - FM2
+                    #        4 - FMAST
+                    #        5 - AM
+                if frame_id == RADIO_SOURCE_FRAME :
+                    #temp = format_data_hex(data)
+                    #if temp == Radio : afficher la tab radio 
+                    #elseif temp == cdc : afficher la page bluetooth ? etc.
+                    True
+                    
+                    
+                    
+                else :    
+                    print ("FRAME ID %s  :  %s  %s"   % (frame_id, format_data_hex(data),format_data_ascii(data)))
         stop_reading.wait()
 
     except:
@@ -104,9 +148,10 @@ def run():
 
         app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
         root = Ui()  # Create an instance of our class
+        
+        #Création du Thread pour la boucle de lecture, args : source_handler pour l'usb et root pour l'UI
         reading_thread = threading.Thread(target=reading_loop, args=(source_handler,root,))
         reading_thread.start()
-        # root.RadioName.setText("placeholder")
         app.exec_()  # Start the application
 
     finally:
