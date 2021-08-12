db27a04a8b3650e49d32a3bcec73e54c700f748c
lucastu <86346920+lucastu@users.noreply.github.com>
2021-08-12 11:25:28 +0200

    Mis à jour pour V2

--- canmonitor/ombre.py
+++ canmonitor/ombre.py
@@ -6,10 +6,10 @@ from PyQt5.QtWidgets import (QApplication, QDialog)
 # Pour le dev : affiche sur l ecran de la raspberry
 os.environ.__setitem__('DISPLAY', ':0.0')
 #Definition de la taille de lecran
-ScreenHeight = 720
-ScreenWidth = 1280
+#ScreenHeight = 720
+#ScreenWidth = 1280
 
-class Actions(QDialog):
+class Ombre(QDialog):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -17,15 +17,14 @@ class Actions(QDialog):
     def initUI(self):
         # Creation de la fenetre
         self.setWindowTitle('Ombre')
-        self.setFixedSize(ScreenWidth, ScreenHeight)
+        #self.setFixedSize(ScreenWidth, ScreenHeight)
         self.setStyleSheet("background-color: black;")
-
         # Mode Frameless
         self.setWindowFlags(Qt.Widget | Qt.FramelessWindowHint)
-        self.show()
+        self.showMaximized()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    shadow = Actions()
-    QTimer.singleShot (7000, shadow.close)
+    shadow = Ombre()
+    #QTimer.singleShot (7000, shadow.close)
     sys.exit(app.exec_())
