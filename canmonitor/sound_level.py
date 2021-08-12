19902ed83192462444a9c3c9f91bd64aa261fa1e
lucastu <86346920+lucastu@users.noreply.github.com>
2021-08-12 10:46:51 +0200

    Update sound_level.py

--- canmonitor/sound_level.py
+++ canmonitor/sound_level.py
@@ -5,26 +5,20 @@ from PyQt5.QtWidgets import (QApplication, QDialog,
                              QProgressBar, QPushButton)
 from PyQt5.QtCore import QTimer
 from PyQt5.QtCore import Qt
-from tinydb import TinyDB, Query
 import threading
 
 # Pour le dev : affiche sur l ecran de la raspberry
 os.environ.__setitem__('DISPLAY', ':0.0')
 
-global Volume
-Volume =0
-#Definition de la taille de lecran
-ScreenHeight = 720
-ScreenWidth = 1280
-# Definition de la taille de la fenetre
-WindowHeight = 100
-WindowWidth =1000
-# Creation dune mini database pour passer des infos entre mes scripts... faute de mieux
-db = TinyDB('/home/pi/lucas/db.json')
-
-class Actions(QDialog):
-
+class volumewindow(QDialog):
     def __init__(self):
+        Volume =0
+        #Definition de la taille de lecran
+        ScreenHeight = 720
+        ScreenWidth = 1280
+        # Definition de la taille de la fenetre
+        WindowHeight = 100
+        WindowWidth =1000
         super().__init__()
         self.initUI()
 
@@ -39,10 +33,6 @@ class Actions(QDialog):
         self.progress.setGeometry(0, 0, WindowWidth, WindowHeight)
         # Maxmimum de valeur de volume
         self.progress.setMaximum(30)
-        # recuperation de la valeur du volume
-        Niveau = db.get(Query()['Nom'] == 'Volume')
-        Volume = int(Niveau.get('Valeur'))
-        self.progress.setValue( Volume )
         # Retire le text du pourcentage dans la barre
         self.progress.setTextVisible(0)
         # Mode Frameless
@@ -83,7 +73,6 @@ class Actions(QDialog):
 
     def movedown(self):
         size = self.size()
-
         # Emplacement de la fenetre en fonction de la taille de l ecran et de la fenetre pour etre centre
         emplacement_y_cible =  ScreenHeight
         emplacement_x = int(ScreenWidth / 2 - size.width() / 2)
@@ -105,28 +94,33 @@ class Actions(QDialog):
         # On se cache
         self.hide()
 
-    def waitforchange(self):
-        Niveau = db.get(Query()['Nom'] == 'Volume')
-        Volume = int(Niveau.get('Valeur'))
-        New_Volume = Volume
-        while 1:
-            Niveau = db.get(Query()['Nom'] == 'Volume')
-            New_Volume = int(Niveau.get('Valeur'))
+    def volume_loop(self):
+          positionup = False
+          while not stop_thread :
+            #Si le flag interne du threald est levé, on enregistre son heure et on descend le flag
+            if self.isset() :
+              lastchangetime = int(time.time())
+              self.clear()
 
-            if Volume != New_Volume:
-                print ("Volume : %s, Nouveau Volume : %s " %(Volume, New_Volume ))
-                Volume = New_Volume
+            #Si ça fait moins de 2 secondes qu'il ya eu un changement
+            if int(time.time()) - lastchangetime < 2 :
+              self.progress.setValue(Volume)
+              if positionup == False :
+                #Volume.show()
                 self.moveup()
-                # On recupere la valeur du volume apres s'être cache pour eviter une relmontee
-                Niveau = db.get(Query()['Nom'] == 'Volume')
-                Volume = int(Niveau.get('Valeur'))
-            time.sleep(1)
-            # print ("attente changement")
+                positionup = True
+            #si ça fait plus de 2 secondes mais que la position de la fenêtre est Up, on la descend
+            elif positionup == True :
+              self.movedown()
+              positionup = False
+            #En dehors de ces cas, on attend la levée du flag
+            else : 
+              self.wait()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Actions()
+    window = VolumeWindow()
     # On lance la boucle qui attend le changment de volume
-    threading.Thread(target=window.waitforchange).start()
+    threading.Thread(target=window.volume_loop).start()
     sys.exit(app.exec_())
