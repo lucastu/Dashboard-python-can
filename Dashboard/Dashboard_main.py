92396b8d10f4a3b89c57f121c0534675f15efb78
lucastu <86346920+lucastu@users.noreply.github.com>
2021-08-16 13:23:57 +0200

    Ajout dark mode & background img

--- Dashboard/Dashboard_main.py
+++ Dashboard/Dashboard_main.py
@@ -294,8 +294,32 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
         uic.loadUi('/home/pi/lucas/interface.ui', self)  # Load the .ui Mainwindow file
+        self.setStyleSheet(„ background-image: url(/home/pi/lucas/bg.jpg);“)
+                  
+        #test darkmode         
+        dark_palette = QPalette()
+        dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.WindowText, Qt.white)
+        dark_palette.setColor(QPalette.Base, QColor(35, 35, 35))
+        dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.ToolTipBase, QColor(25, 25, 25))
+        dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+        dark_palette.setColor(QPalette.Text, Qt.white)
+        dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.ButtonText, Qt.white)
+        dark_palette.setColor(QPalette.BrightText, Qt.red)
+        dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        dark_palette.setColor(QPalette.HighlightedText, QColor(35, 35, 35))
+        dark_palette.setColor(QPalette.Active, QPalette.Button, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.darkGray)
+        dark_palette.setColor(QPalette.Disabled, QPalette.WindowText, Qt.darkGray)
+        dark_palette.setColor(QPalette.Disabled, QPalette.Text, Qt.darkGray)
+        dark_palette.setColor(QPalette.Disabled, QPalette.Light, QColor(53, 53, 53))          
+        self.setPalette(dark_palette)           
+         #FIN TEST        
+        self.closebutton.clicked.connect(self.close_all)                  
         self.showMaximized()  # Show the GUI
-        self.closebutton.clicked.connect(self.close_all)
                       
     def close_all(self):
         #on indique qu'on souhaite fermer              
