318f77f86425082866f6228ccf08108f029d5169
lucastu <86346920+lucastu@users.noreply.github.com>
2021-08-17 13:02:51 +0200

    Click anywhere close frame

--- Dashboard/ombre.py
+++ Dashboard/ombre.py
@@ -5,14 +5,15 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QDialog)
 os.environ.__setitem__('DISPLAY', ':0.0')
 from PyQt5 import QtWidgets, uic
-class Ombre(QtWidgets.QDialog):
+
+class ombre(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         # Creation de la fenetre
-        self.setWindowTitle('Opppmbre')
+        #self.setWindowTitle('shadow')
         #self.setFixedSize(ScreenWidth, ScreenHeight)
         self.setStyleSheet("background-color: black;")
         self.setWindowOpacity(0.5)
@@ -20,6 +21,9 @@ class Ombre(QtWidgets.QDialog):
         self.setWindowFlags(Qt.Widget | Qt.FramelessWindowHint)
         self.showMaximized()
 
+     def mousePressEvent(self, QMouseEvent):
+        self.close()
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     shadow = Ombre()
