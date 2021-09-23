f5adc99828f555f957309ea036c1268a8ffd5c10
lucastu <86346920+lucastu@users.noreply.github.com>
2021-09-23 15:32:38 +0200

    clean code

--- Arduino_can_final/Arduino_can_final/Arduino_can_final.ino
+++ Arduino_can_final/Arduino_can_final/Arduino_can_final.ino
@@ -114,29 +114,7 @@ void setup() {
     CAN.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
     pinMode(CAN0_INT, INPUT);
     // Si on est ok envoi de la trame check
-    Serial.println("FRAME:ID=0:LEN=8:00:00:00:00:00:00:00:00");
-    // Envoi du message pour activer la sortie CDC
-    // Envoi du message pour activer la sortie CDC => KO
-    //Essai en envoyant tout !    
-      byte dataInit[] = { 0x92, 0x00 };
-      CAN.sendMsgBuf(416, 0, 2, dataInit);
-      delay(200);
-      byte dataDisk1[] = { 0x20, 0x01, 0x06, 0x05, 0x00, 0x10, 0x00};
-      CAN.sendMsgBuf(354, 0, 7, dataDisk1);
-      delay(200);
-      byte dataDisk[] = { 0x00, 0x00, 0x00, 0x00, 0x33, 0x33, 0x03, 0x33};
-      CAN.sendMsgBuf(46, 0, 8, dataDisk);
-      delay(200);
-      byte datatRACK[] = { 0x01, 0x58, 0x01, 0x0 , 0x00, 0x00 };
-      CAN.sendMsgBuf(418, 0, 5, datatRACK);
-      delay(200);
-      byte dataUNKN[] = { 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 , 0x00 };
-      CAN.sendMsgBuf(1329, 0, 8, dataUNKN);
-      delay(200);
-      byte dataUNKN2[] = { 0x31, 0x07, 0x01, 0x05, 0x02, 0x04, 0x20, 0x09 };
-      CAN.sendMsgBuf(1521, 0, 8, dataUNKN2);    
-      delay(200);
-      Serial.println("Done INIT ?");
+    Serial.println("FRAME:ID=0:LEN=8:00:00:00:00:00:00:00:00");      
   }
 }
 
