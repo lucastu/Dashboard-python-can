521e5f88137f72c9d2b120cb6dcc3fbc84fbb2a4
lucastu <86346920+lucastu@users.noreply.github.com>
2021-09-28 13:42:47 +0200

    Edit logging filemod

--- Dashboard/Dashboard_main.py
+++ Dashboard/Dashboard_main.py
@@ -38,8 +38,7 @@ thread_exception = None
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='/home/pi/lucas/log.txt',
-                    filemode='w')
+                    filename='/home/pi/lucas/log.txt')
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
