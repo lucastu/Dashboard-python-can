fd8ba426b6dac6a95d9ca906f35d01a096787159
lucastu <86346920+lucastu@users.noreply.github.com>
2021-11-04 15:19:28 +0100

    Cleanup

--- Arduino_can_final/Arduino_cdc_source_activation_only/Arduino_cdc_source_activation_only.ino
+++ Arduino_can_final/Arduino_cdc_source_activation_only/Arduino_cdc_source_activation_only.ino
@@ -14,7 +14,6 @@ const long SERIAL_SPEED = 115200;
 
 // CAN bus data rate
 const byte CAN_SPEED = CAN_125KBPS;
-#define CAN0_INT 2  
 ///////////////////
 
 unsigned long lastCDCactivation =0;
@@ -25,8 +24,6 @@ void setup() {
   
   if (CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK) {
     CAN.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-    pinMode(CAN0_INT, INPUT);
-
   }
 }
 
@@ -41,22 +38,4 @@ void loop() {
         lastCDCactivation = millis();
         Serial.println("Data sent");
       }
-
-
-      //Used to print all Can data received
-      unsigned char len = 0;
-      byte buffer[8];
-      long unsigned int id;
-      
-      if (CAN.checkReceive() == CAN_MSGAVAIL) {
-        CAN.readMsgBuf(&id, &len, buffer);
-        Serial.print("ID: "+ String(id) + " len : "+len+ " data : "); 
-        for(int i=0; i<=len; i++) 
-         {
-          Serial.print(buffer[i], HEX);
-          Serial.print(" ");
-         }
-        Serial.println();
-        }
-
-      }
+}
