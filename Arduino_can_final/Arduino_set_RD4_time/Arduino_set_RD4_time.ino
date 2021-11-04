826f3a7b4331bcb05baa93749bed5e1ba1084205
lucastu <86346920+lucastu@users.noreply.github.com>
2021-11-04 09:48:37 +0100

    Update Arduino_set_RD4_time.ino

--- Arduino_can_final/Arduino_set_RD4_time/Arduino_set_RD4_time.ino
+++ Arduino_can_final/Arduino_set_RD4_time/Arduino_set_RD4_time.ino
@@ -14,7 +14,7 @@ const long SERIAL_SPEED = 115200;
 
 // CAN bus data rate
 const byte CAN_SPEED = CAN_125KBPS;
-#define CAN0_INT 2  
+
 ///////////////////
 
 unsigned long lastCDCactivation =0;
@@ -28,35 +28,11 @@ void setup() {
     pinMode(CAN0_INT, INPUT);
 
   }
+  byte datadate[] = {0x00, 0x00, 0x00, 0x00, 0x00};
+  CAN.sendMsgBuf(859, 0, 5, datadate);
 }
 
 
 void loop() {
-      Serial.println(millis()-lastCDCactivation);
-      if (millis()-lastCDCactivation >=100){
-        byte dataInit[] = {0x20, 0x01, 0x06, 0x05, 0x00, 0x08, 0x00};
-        CAN.sendMsgBuf(354, 0, 7, dataInit);
-        byte dataInit2[] = {0x01, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00};
-        CAN.sendMsgBuf(482, 0, 7, dataInit2);
-        lastCDCactivation = millis();
-        Serial.println("Data sent");
-      }
-
-
-      //Used to print all Can data received
-      unsigned char len = 0;
-      byte buffer[8];
-      long unsigned int id;
-      
-      if (CAN.checkReceive() == CAN_MSGAVAIL) {
-        CAN.readMsgBuf(&id, &len, buffer);
-        Serial.print("ID: "+ String(id) + " len : "+len+ " data : "); 
-        for(int i=0; i<=len; i++) 
-         {
-          Serial.print(buffer[i], HEX);
-          Serial.print(" ");
-         }
-        Serial.println();
-        }
-
+      delay(1000000);
       }
